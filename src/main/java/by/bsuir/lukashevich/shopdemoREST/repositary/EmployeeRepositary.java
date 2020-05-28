2b6f1cc290b8f9150a22085337b6bc0f66c3ea56
Luiryna <luiryna322@gmail.com>
2020-05-29 02:11:22 +0300

    rest is ready, I hope

--- src/main/java/by/bsuir/lukashevich/shopdemoREST/repositary/EmployeeRepositary.java
+++
@@ -1,7 +0,0 @@
-package by.bsuir.lukashevich.shopdemoREST.repositary;
-
-import by.bsuir.lukashevich.shopdemoREST.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepositary extends JpaRepository<Employee, Long> {
-}
--- src/main/java/by/bsuir/lukashevich/shopdemoREST/repositary/PriceRepositary.java
+++
@@ -1,7 +0,0 @@
-package by.bsuir.lukashevich.shopdemoREST.repositary;
-
-import by.bsuir.lukashevich.shopdemoREST.model.Price;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PriceRepositary extends JpaRepository<Price, Long> {
-}
